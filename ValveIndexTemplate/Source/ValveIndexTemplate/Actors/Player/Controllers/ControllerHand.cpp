5882812d458b56b934bd12242e254e4c2562409c
caelrowley <caelrowley@gmail.com>
2021-05-04 19:32:26 +0200

    feat: add new controller mesh and fix bug with controls

--- ValveIndexTemplate/Source/ValveIndexTemplate/Actors/Player/Controllers/ControllerHand.cpp
+++ ValveIndexTemplate/Source/ValveIndexTemplate/Actors/Player/Controllers/ControllerHand.cpp
@@ -3,6 +3,26 @@
 
 #include "ControllerHand.h"
 
+// Sets default values
+AControllerHand::AControllerHand()
+{
+	MotionController->SetShowDeviceModel(false);
+}
+
+void AControllerHand::Initialize(EControllerHand TrackingSource)
+{
+	Super::Initialize(TrackingSource);
+
+	if (MotionController->GetTrackingSource() == EControllerHand::Left) {
+		MotionController->SetRelativeScale3D(FVector(1.0, -1.0, 1.0));
+		UE_LOG(LogTemp, Warning, TEXT("SetRelativeScale3D: LEFT"));
+	}
+
+	if (MotionController->GetTrackingSource() == EControllerHand::Right) {
+		UE_LOG(LogTemp, Warning, TEXT("SetRelativeScale3D: RIGHT"));
+	}
+}
+
 void AControllerHand::ThumbstickX(float axisValue, APawn* pawn, FVector direction)
 {
 	Super::ThumbstickX(axisValue, pawn, direction);
--- ValveIndexTemplate/Source/ValveIndexTemplate/Actors/Player/Controllers/ControllerHand.h
+++ ValveIndexTemplate/Source/ValveIndexTemplate/Actors/Player/Controllers/ControllerHand.h
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include "Engine/SkeletalMesh.h"
+
 #include "CoreMinimal.h"
 #include "ValveIndexTemplate/Actors/Player/Controllers/ControllerInterface.h"
 #include "ControllerHand.generated.h"
@@ -15,6 +17,10 @@ class VALVEINDEXTEMPLATE_API AControllerHand : public AControllerInterface
 	GENERATED_BODY()
 
 public:
+	AControllerHand();
+
+	virtual void Initialize(EControllerHand TrackingSource) override;
+
 	void ThumbstickX(float axisValue, APawn* pawn, FVector direction) override;
 	void ThumbstickY(float axisValue, APawn* pawn, FVector direction) override;
 };
